ddbfe17d71a1bf62919db8a6eb1abe3ac73a2a19
Jeff Ripke <jripke74@gmail.com>
2025-04-02 12:02:17 -0700

    adjust h1 top/bottom margin

---
+++ script.js
@@ -0,0 +1,21 @@
+// Step 1
+// In this workshop, you will continue to practice working with the 
+// DOM and event handling by building a Rock, Paper, Scissors game.
+
+// The HTML and CSS boilerplate for the game has been provided for 
+// you.
+
+// On the screen, you will see the rules to the game, the player 
+// and computer scores and the buttons for the user to select Rock, 
+// Paper, or Scissors.
+
+// Your task over the next few steps is to build the JavaScript 
+// functionality that generates a computer choice, compares it to 
+// the player's choice, and updates the scores accordingly.
+
+// Take some time to look through the HTML provided and familiarize 
+// yourself with the structure of the game.
+
+// When you're ready, click on the "Check your code" button to 
+// proceed to the next step and start the workshop.
+
